98a0d349117b5de9e288e0e6cffbcdf7f98f682c
TypicalCrusader <janflisek@gmail.com>
2024-10-29 16:04:59 +0100

    Item lock

--- Wizardry/Core/BattleSys/WeaponLockEx/Source/data.c
+++ Wizardry/Core/BattleSys/WeaponLockEx/Source/data.c
@@ -4,8 +4,49 @@
 
 const struct WeaponLockExConf gWeaponLockExConfs[] = {
 	{
-		.iid = ITEM_SWORD_RAPIER,
-		.pid = CHARACTER_EIRIKA,
+		//Sealed Tyfang - August - Heavy
+		.iid = ITEM_SWORD_MKATTI,
+		.pid = CHARACTER_EIRIKA, 
 	},
+	{
+		//Sealed Tyfang - August - Light
+		.iid = ITEM_SWORD_MKATTI,
+		.pid = CHARACTER_EPHRAIM, 
+	},	
+	{
+		//Unsealed Tyfang - August - Heavy
+		.iid = ITEM_SWORD_AUDHULMA,
+		.pid = CHARACTER_EIRIKA, 
+	},	
+	{
+		//Unsealed Tyfang - August - Light
+		.iid = ITEM_SWORD_AUDHULMA,
+		.pid = CHARACTER_EPHRAIM, 
+	},	
+	{
+		//Unsealed Tyfang - Ascalon
+		.iid = ITEM_SWORD_AUDHULMA,
+		.pid = CHARACTER_VALTER_PROLOGUE, //last BWL value
+	},		
+	{
+		//Liberated Tyfang - August - Heavy
+		.iid = ITEM_LANCE_SIEGMUND,
+		.pid = CHARACTER_EIRIKA, 
+	},	
+	{
+		//Liberated Tyfang - August - Light
+		.iid = ITEM_LANCE_SIEGMUND,
+		.pid = CHARACTER_EPHRAIM, 
+	},		
+	{
+		//Sealed Ultima - Cyric
+		.iid = ITEM_DARK_NAGLFAR,
+		.pid = CHARACTER_SETH, 
+	},			
+	{
+		//Unsealed Ultima - Cyric
+		.iid = ITEM_AXE_GARM,
+		.pid = CHARACTER_SETH, 
+	},		
 	{}
 };
