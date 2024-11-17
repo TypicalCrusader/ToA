e144605700588adb24cf3d21456a80e7f028a151
TypicalCrusader <janflisek@gmail.com>
2024-11-18 00:39:52 +0100

    eg

--- Wizardry/TypicalCHax/CBattleTransform/src/Data.c
+++ Wizardry/TypicalCHax/CBattleTransform/src/Data.c
@@ -1,7 +1,17 @@
 #include "Data.h"
+#include "constants\TOA_items.h"
 
-const struct RaceStruct gTranformStruct[] = {
+const struct TransformTable gTranformStruct[] = {
     {
-        
-    }
+        .jid = CLASS_MANAKETE,
+        .iid = 0x0,
+        .gMainAnim= 0x0,
+        .gIntroAnim= 0x0,
+        .gExitAnim= 0x0,
+        .gNoWPNAnim= 0x0,
+        .TransformStartSound= 0x0,
+        .TransformFinishSound= 0x0,
+        .UntransformSound= 0x0,
+    },
+    {}
 }
--- Wizardry/TypicalCHax/CBattleTransform/src/Data.h
+++ Wizardry/TypicalCHax/CBattleTransform/src/Data.h
@@ -3,10 +3,10 @@
 struct TransformTable {
     u8 jid; //class
     u16 iid; //item
-    u16 gMainAnim;
-    u16 gIntroAnim;
-    u16 gExitAnim;
-    u16 gNoWPNAnim;
+    u8 gMainAnim;
+    u8 gIntroAnim;
+    u8 gExitAnim;
+    u8 gNoWPNAnim;
     u16 TransformStartSound;
     u16 TransformFinishSound;
     u16 UntransformSound;
